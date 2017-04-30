578ff395e8b293d1f9afeeca55afbebf8f3c92a0
Swizz <gerodel.quentin@gmail.com>
2017-04-30 17:56:58 +0200

    Add more rules and fix some troubles

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-'use strict';
+'use strict'
+
 module.exports = {
-	extends: ['xo-react'],
+  extends: ['xo-react'],
 
-	semicolon: false,
-	space: true,
+  semicolon: false,
+  space: true,
 
-	settings: {
-		react: {
-			pragma: 'Snabbdom'
-		}
-	},
+  settings: {
+    react: {
+      pragma: 'Snabbdom'
+    }
+  },
 
-	rules: {
-		'padded-blocks': ['error', 'always'],
-		'object-curly-spacing': ['error', 'always'],
-		'brace-style': ['error', '1tbs', {allowSingleLine: true}],
-		'new-cap': ['error', {capIsNew: false}],
-		'no-unused-vars': ['error', {varsIgnorePattern: '^_', argsIgnorePattern: '^_'}],
-		'dot-location': ['error', 'object'],
-		'react/jsx-curly-spacing': [2, 'always'],
-		'react/jsx-no-bind': [2, {allowArrowFunctions: true}],
-		'react/jsx-closing-bracket-location': [2, 'tag-aligned'],
-		'react/jsx-pascal-case': 2
-	}
-};
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-parens': ['error', 'always'],
+    'brace-style': ['error', '1tbs', {allowSingleLine: true}],
+    'new-cap': ['error', {capIsNew: false}],
+    'no-unused-vars': ['error', {varsIgnorePattern: '^_', argsIgnorePattern: '^_'}],
+    'dot-location': ['error', 'object'],
+    'no-negated-condition': 'off',
+
+    'react/jsx-curly-spacing': ['error', 'always'],
+    'react/jsx-no-bind': ['error', {allowArrowFunctions: true}],
+    'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
+    'react/self-closing-comp': ['error', {component: true, html: false}],
+    'react/jsx-pascal-case': 'error',
+    'react/prop-types': 'off',
+
+    'import/no-named-as-default-member': 'off',
+    'import/prefer-default-export': 'off'
+  }
+}
