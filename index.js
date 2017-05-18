bdf7fd426750e982ba16702aeec660bdfd7186e4
Swizz <gerodel.quentin@gmail.com>
2017-05-18 18:27:55 +0200

    Update rules and dependencies

--- index.js
+++ index.js
@@ -15,18 +15,31 @@ module.exports = {
   rules: {
     'object-curly-spacing': ['error', 'always'],
     'arrow-parens': ['error', 'always'],
-    'brace-style': ['error', '1tbs', {allowSingleLine: true}],
-    'new-cap': ['error', {capIsNew: false}],
-    'no-unused-vars': ['error', {varsIgnorePattern: '^_', argsIgnorePattern: '^_'}],
+    'brace-style': ['error', '1tbs', {
+      allowSingleLine: true
+    }],
+    'new-cap': ['error', {
+      capIsNew: false
+    }],
+    'no-unused-vars': ['error', {
+      varsIgnorePattern: '^_',
+      argsIgnorePattern: '^_',
+      ignoreRestSiblings: true
+    }],
     'dot-location': ['error', 'object'],
     'no-negated-condition': 'off',
 
     'react/jsx-curly-spacing': ['error', 'always'],
-    'react/jsx-no-bind': ['error', {allowArrowFunctions: true}],
+    'react/jsx-no-bind': ['error', {
+      allowArrowFunctions: true
+    }],
     'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
-    'react/self-closing-comp': ['error', {component: true, html: false}],
-    'react/jsx-pascal-case': 'error',
+    'react/self-closing-comp': ['error', {
+      component: true,
+      html: false
+    }],
     'react/prop-types': 'off',
+    'react/jsx-key': 'off',
 
     'import/no-named-as-default-member': 'off',
     'import/prefer-default-export': 'off'
